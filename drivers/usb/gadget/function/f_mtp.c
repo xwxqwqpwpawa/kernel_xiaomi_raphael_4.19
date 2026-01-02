e21c3a2447e70bc76530a2dae61a6759f91cbf98
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2026-01-02 14:46:12 +0800

    Revert "usb: gadget: function: f_mtp: add missed change"
    This reverts commit 71612fc4049a29703d737537853d7a53a031a4e6.
    
    Signed-off-by: Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -617,9 +617,6 @@ static ssize_t mtp_read(struct file *fp, char __user *buf,
 		goto wait_err;
 	}
 
-#if defined(CONFIG_MACH_XIAOMI_VAYU)
-	cdev = dev->cdev;
-#endif
 	len = ALIGN(count, dev->ep_out->maxpacket);
 	if (len > mtp_rx_req_len)
 		return -EINVAL;
